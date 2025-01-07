d4525758bfa0ef12609f05a75cd4d077e031d4f6
antn9x <antn9x@gmail.com>
2025-01-08 06:29:42 +0700

    fix lint

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+/* eslint-disable no-undef */
 const collideEvents = ['onCollisionEnter', 'onCollisionExit', 'onCollisionStay']
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
 function camelCase(str) {
-  return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
-    return index === 0 ? word.toLowerCase() : word.toUpperCase();
-  }).replace(/\s+/g, '');
+  return str
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+      return index === 0 ? word.toLowerCase() : word.toUpperCase()
+    })
+    .replace(/\s+/g, '')
 }
 const nameCount = {}
 function getComponentName(name = '') {
-  if (!nameCount[name])
-    nameCount[name] = 0
+  if (!nameCount[name]) nameCount[name] = 0
   return `${camelCase(name)}Comp${++nameCount[name]}`
 }
 
@@ -45,12 +47,14 @@ function parseExpression(expression) {
       return extra.raw
     case 'MemberExpression':
       return `${object.name}.${property.name}`
-    case 'CallExpression':
-      const { callee, arguments } = expression
-      return `${callee.name}(${arguments.map(parseValue).join(', ')})`
-    case 'UnaryExpression':
-      const { operator, argument } = expression
-      return `${operator}${parseValue(argument)}`;
+    case 'CallExpression': {
+      const { callee, arguments: args } = expression
+      return `${callee.name}(${args.map(parseValue).join(', ')})`
+    }
+    case 'UnaryExpression': {
+      const { operator, argument: args } = expression
+      return `${operator}${parseValue(args)}`
+    }
     default:
       console.log('not support parseExpression', type)
   }
@@ -59,9 +63,11 @@ function parseExpression(expression) {
 function parseAttribute(value, componentVar, prop) {
   if (value.type === 'JSXExpressionContainer' && value.expression.type === 'ObjectExpression') {
     const { properties } = value.expression
-    return properties.map(p => {
-      return `\n    ${componentVar}.${prop}.${p.key.name} = ${parseExpression(p.value)}`
-    }).join('')
+    return properties
+      .map((p) => {
+        return `\n    ${componentVar}.${prop}.${p.key.name} = ${parseExpression(p.value)}`
+      })
+      .join('')
   }
   return `\n    ${componentVar}.${prop} = ${parseValue(value)}`
 }
@@ -70,34 +76,43 @@ function attributesToParams(attributes) {
   let props = ''
   attributes.map(({ name, value }) => {
     const attName = name.name
-    if (attName === 'node' || attName.includes('$')) return;
+    if (attName === 'node' || attName.includes('$')) return
     props += `${attName}: ${parseValue(value)},`
   })
   return `{${props}}`
 }
 
-let currentClassName;
-let hasStart = false;
-let hadJSX = false;
+function parseJSXExpressionContainer(element) {
+  const { expression } = element
+  const { type } = expression
+  if (type === 'JSXEmptyExpression') {
+    return
+  }
+  if (type === 'CallExpression') {
+    const { callee, arguments: args } = expression
+    console.log(callee, args)
+  }
+}
+
+let currentClassName
+let hasStart = false
+// let hadJSX = false
 let register = ''
 module.exports = function ({ types: t }) {
   return {
-    pre(state) {
+    pre() {
       register = ''
     },
     visitor: {
       ImportDeclaration(path) {
         // console.log(path.node)
         const { specifiers, source } = path.node
-        if (source.value === '@safe-engine/pixi' ||
-          source.value === 'safex' ||
-          source.value === '@safex/cocos'
-        ) {
-          const identifier = t.identifier('registerSystem');
-          path.pushContainer('specifiers', identifier);
+        if (source.value === '@safe-engine/pixi' || source.value === 'safex' || source.value === '@safex/cocos') {
+          const identifier = t.identifier('registerSystem')
+          path.pushContainer('specifiers', identifier)
         }
         if (source.value.includes('component')) {
-          specifiers.forEach(sp => {
+          specifiers.forEach((sp) => {
             const componentName = sp.local.name
             const newReg = `registerSystem(${componentName});`
             if (!register.includes(newReg)) {
@@ -107,15 +122,13 @@ module.exports = function ({ types: t }) {
         }
       },
       ExportDeclaration(path) {
-        hasStart = false;
-        if (path.node.declaration && path.node.declaration.id)
-          currentClassName = path.node.declaration.id.name
+        hasStart = false
+        if (path.node.declaration && path.node.declaration.id) currentClassName = path.node.declaration.id.name
       },
       ClassDeclaration(path) {
         // console.log(path.node.body.body)
-        hasStart = false;
-        if (!currentClassName)
-          currentClassName = path.node.id.name
+        hasStart = false
+        if (!currentClassName) currentClassName = path.node.id.name
       },
       ClassMethod(path) {
         // console.log(path.node.key.name)
@@ -124,16 +137,16 @@ module.exports = function ({ types: t }) {
         }
       },
       JSXElement(path) {
-        hadJSX = true;
+        // hadJSX = true
         const { openingElement, children } = path.node
         const { attributes, name: rootTag } = openingElement
         let ret = ''
-        let refs = '';
-        let begin = `${register}registerSystem(${currentClassName});`;
+        let refs = ''
+        let begin = `${register}registerSystem(${currentClassName});`
         const classVar = getComponentName(currentClassName)
         function parseJSX(tagName, children, attributes = [], parentVar) {
           const componentName = tagName.name
-          // console.log('parseJSX', componentName)
+          console.log('parseJSX', componentName)
           if (componentName === 'ExtraDataComp') {
             // console.log(parentVar, attributes[1])
             const key = attributes.find(({ name }) => name.name === 'key').value.value
@@ -166,12 +179,10 @@ module.exports = function ({ types: t }) {
                 rightValue = `${compVar}.getComponent(${compName})`
                 leftVar = `${classVar}.${refVal}`
               }
-              if (isPushList)
-                refs += `\n${leftVar}.push(${rightValue});`
-              else
-                refs += `\n${leftVar} = ${rightValue};`
-            } else if (attName === '$node') {
-              refs += `\n${classVar}.${value.value} = ${compVar}.${cbName};`
+              if (isPushList) refs += `\n${leftVar}.push(${rightValue});`
+              else refs += `\n${leftVar} = ${rightValue};`
+              // } else if (attName === '$node') {
+              //   refs += `\n${classVar}.${value.value} = ${compVar}.${cbName};`
             } else if (attName.includes('$')) {
               const cbName = attName.replace('$', '')
               let bindVal
@@ -184,15 +195,20 @@ module.exports = function ({ types: t }) {
               if (collideEvents.includes(cbName)) {
                 refs += `\n${compVar}.set${capitalizeFirstLetter(cbName)}(${classVar}.${value.value}.bind(${bindVal}));`
               } else {
-                refs += `\n${compVar}.${(cbName)}=${classVar}.${value.value}.bind(${bindVal});`
+                refs += `\n${compVar}.${cbName}=${classVar}.${value.value}.bind(${bindVal});`
               }
             } else if (attName === 'node') {
               ret += parseAttribute(value, compVar, attName)
             }
           })
-          children.forEach(element => {
+          children.forEach((element) => {
             const { openingElement, children, type } = element
-            if (type !== 'JSXElement') return;
+            if (type !== 'JSXElement') {
+              if (type === 'JSXExpressionContainer') {
+                parseJSXExpressionContainer(element)
+              }
+              return
+            }
             const { attributes, name } = openingElement
             parseJSX(name, children, attributes, compVar)
           })
@@ -206,10 +222,10 @@ module.exports = function ({ types: t }) {
         path.replaceWithSourceString(`function () {
           ${begin}
           ${ret}
-        }()`);
+        }()`)
         // console.log(path.node)
         path.parentPath.parentPath.replaceWith(path.node.callee.body)
-      }
+      },
     },
   }
 }
